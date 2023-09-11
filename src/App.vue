a27d655eefe3db495e2955ad9cb549d66f5b576b
Potingtw <124490373+Potingtw@users.noreply.github.com>
2023-09-11 08:47:16 +0000

    upload

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,72 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+    import { ref } from 'vue';
+    export default {
+        name: 'App',
+        setup () {
+            const newTodo = ref('');
+            const defaultData = [{
+                done: false,
+                content: 'Write a blog post'
+            }]
+            const todosData = JSON.parse(localStorage.getItem('todos')) ||                         defaultData;
+            const todos = ref(todosData);
+            function addTodo () {
+                if (newTodo.value) {
+                    todos.value.push({
+                        done: false,
+                        content: newTodo.value
+                    });
+                    newTodo.value = '';
+                }
+                saveData();
+            }
+            function doneTodo (todo) {
+                todo.done = !todo.done
+                saveData();
+            }
+            function removeTodo (index) {
+                todos.value.splice(index, 1);
+                saveData();
+            }
+            function saveData () {
+                const storageData = JSON.stringify(todos.value);
+                localStorage.setItem('todos', storageData);
+            }
+            return {
+                todos,
+                newTodo,
+                addTodo,
+                doneTodo,
+                removeTodo,
+                saveData
+            }
+        }
+    }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <h1>ToDoList</h1>
+    <form @submit.prevent="addTodo()">
+        <label>代辦事項 </label>
+        <input
+            v-model="newTodo"
+            name="newTodo"
+            autocomplete="off"
+        >
+        <button>新增</button>
+    </form>
+    <h2>未完成事項</h2>
+    <ul>
+        <li
+            v-for="(todo, index) in todos"
+            :key="index"
+        >
+            <span
+                :class="{ done: todo.done }"
+                @click="doneTodo(todo)"
+            >{{ todo.content }}</span>
+            <button @click="removeTodo(index)">移除</button>
+        </li>
+    </ul>
+    <h4 v-if="todos.length === 0">Empty list.</h4>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
